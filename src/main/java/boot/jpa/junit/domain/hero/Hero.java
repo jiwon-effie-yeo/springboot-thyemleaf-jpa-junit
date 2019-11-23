cfc5fb7d6cabc5e451d5cf6cbeefc8ee536b48c3
DESKTOP-A828S0O\jiwon <C:\Users\\Documents\git_SourceTree>
2019-11-23 11:43:17 +0900

    Create RepositoryTest-JPA Auditing

--- src/main/java/boot/jpa/junit/domain/hero/Hero.java
+++ src/main/java/boot/jpa/junit/domain/hero/Hero.java
@@ -18,7 +18,7 @@ public class Hero extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 20)
+    @Column(length = 40)
     private String name;
 
     @Column
